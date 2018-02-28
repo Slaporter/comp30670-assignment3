7c3945f8eebec273344622f7e6afc69c7b35eb99
Shauna Porter <shauna.porter@ucdconnect.ie>
2018-02-28 20:41:32 +0000

    Testing parse file function with sample inputs

---
+++ countMeIn/lightTester.py
@@ -0,0 +1,24 @@
+'''
+Created on 28 Feb 2018
+
+@author: Slaporter
+'''
+
+class lightTester:
+    lights=None
+    
+    def __init__(self,n):
+        self.lights=0
+        
+    def apply(self, cmd):
+        if cmd is "switch":
+           pass 
+        elif cmd is "turn on":
+            pass
+        elif cmd is "turn off":
+            pass
+        else:
+            pass
+        
+    def count(self):
+        return 
--- countMeIn/main.py
+++ countMeIn/main.py
@@ -3,15 +3,28 @@ Created on 28 Feb 2018
 
 @author: Slaporter
 '''
+import re
+from countMeIn.lightTester import lightTester
+
+
 def main(filename):
     instructions=parse_file(filename)
     lights=lightTester(instructions[0])
-    for cmd in instructions:
+    for cmd in instructions[1:]:
         lights.apply(cmd)
     print("Number of lights on:",lights.count())
     
 def parse_file(filename):
-    pass
-
-
+    fh1=open(filename).read().splitlines()
+    rex = re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*") 
+    my_list=[fh1[0]]
+    for line in fh1[1:]:
+        rexy = rex.match(line)
+        my_cmd=[]
+        for i in range(1,6):
+            my_cmd.append(rexy.group(i))
+        my_list.append(my_cmd)
+    print(my_list)
+    
+parse_file("../input_test")
     
