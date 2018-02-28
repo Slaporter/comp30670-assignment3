aa487b84088942da147f7feb2c7d31a24cc5631a
Shauna Porter <shauna.porter@ucdconnect.ie>
2018-02-28 23:01:47 +0000

    Created Apply Function

--- countMeIn/lightTester.py
+++ countMeIn/lightTester.py
@@ -8,17 +8,32 @@ class lightTester:
     lights=None
     
     def __init__(self,n):
-        self.lights=0
+        self.lights = [[False]*n for _ in range(n)]
+        self.count = 0
         
     def apply(self, cmd):
-        if cmd is "switch":
-           pass 
-        elif cmd is "turn on":
-            pass
-        elif cmd is "turn off":
-            pass
-        else:
-            pass
+        if cmd[0] is "switch":
+            for i in range(cmd[1],cmd[3]+1):
+                for j in range(cmd[2],cmd[4]+1):
+                    if self.lights[i][j]==False:
+                        self.count +=1
+                        self.lights[i][j]=True
+                    else:
+                        self.count-=1
+                        self.lights[i][j]=False
+        elif cmd[0] is "turn on":
+            for i in range(cmd[1],cmd[3]+1):
+                for j in range(cmd[2],cmd[4]+1):
+                    if self.lights[i][j]==False:
+                        self.count +=1
+                        self.lights[i][j]=True     
+        elif cmd[0] is "turn off":
+            for i in range(cmd[1],cmd[3]+1):
+                for j in range(cmd[2],cmd[4]+1):
+                    if self.lights[i][j]==True:
+                        self.count -=1
+                        self.lights[i][j]=False 
+
         
     def count(self):
-        return 
+        return self.count
--- countMeIn/main.py
+++ countMeIn/main.py
@@ -12,19 +12,23 @@ def main(filename):
     lights=lightTester(instructions[0])
     for cmd in instructions[1:]:
         lights.apply(cmd)
-    print("Number of lights on:",lights.count())
+    print("Number of lights on:", lights.count)
     
 def parse_file(filename):
     fh1=open(filename).read().splitlines()
     rex = re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*") 
-    my_list=[fh1[0]]
+    my_list=[int(fh1[0])]
     for line in fh1[1:]:
         rexy = rex.match(line)
         my_cmd=[]
         for i in range(1,6):
-            my_cmd.append(rexy.group(i))
+            if i==1:
+                my_cmd.append(rexy.group(i))
+            else:
+                my_cmd.append(int(rexy.group(i)))    
         my_list.append(my_cmd)
-    print(my_list)
+    return my_list
     
 parse_file("../input_test")
+main("../input_test")
     
