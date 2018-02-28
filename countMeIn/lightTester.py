7b85e169cc1e003945f6f35a6143dcb04f7976d1
Shauna Porter <shauna.porter@ucdconnect.ie>
2018-02-28 23:09:39 +0000

    Fixed Apply Function

--- countMeIn/lightTester.py
+++ countMeIn/lightTester.py
@@ -12,24 +12,24 @@ class lightTester:
         self.count = 0
         
     def apply(self, cmd):
-        if cmd[0] is "switch":
-            for i in range(cmd[1],cmd[3]+1):
-                for j in range(cmd[2],cmd[4]+1):
+        if cmd[0] == "switch":
+            for i in range(cmd[1],cmd[3]):
+                for j in range(cmd[2],cmd[4]):
                     if self.lights[i][j]==False:
                         self.count +=1
                         self.lights[i][j]=True
                     else:
                         self.count-=1
                         self.lights[i][j]=False
-        elif cmd[0] is "turn on":
-            for i in range(cmd[1],cmd[3]+1):
-                for j in range(cmd[2],cmd[4]+1):
+        elif cmd[0] == "turn on":
+            for i in range(cmd[1],cmd[3]):
+                for j in range(cmd[2],cmd[4]):
                     if self.lights[i][j]==False:
                         self.count +=1
                         self.lights[i][j]=True     
-        elif cmd[0] is "turn off":
-            for i in range(cmd[1],cmd[3]+1):
-                for j in range(cmd[2],cmd[4]+1):
+        elif cmd[0] == "turn off":
+            for i in range(cmd[1],cmd[3]):
+                for j in range(cmd[2],cmd[4]):
                     if self.lights[i][j]==True:
                         self.count -=1
                         self.lights[i][j]=False 
