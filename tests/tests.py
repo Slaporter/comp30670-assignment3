3f481d03c25c358cb8bab34160620c99bbdab3ab
Shauna Porter <shauna.porter@ucdconnect.ie>
2018-03-08 14:51:23 +0000

    Tidy up test file

--- tests/tests.py
+++ tests/tests.py
@@ -10,17 +10,12 @@ sys.path.append(".")
 from countMeIn.main import main, parse_file, script_run
 from countMeIn.lightTester import lightTester
 
-# def test_main():
-#     assert 4==4
     
 def test_parse_file():
     answer=[10, ['turn on', 0,0,9,9], ['turn off', 0,0, 9,9], ['switch', 0,0,9,9], ['turn off', 0,0,9,9], ['turn on', 2,2,7,7]]
     assert answer==parse_file('input_test')
 
-#     
-# def test_script_run():
-#     assert
-#     
+     
 def test_turn_on():
     lt=lightTester(10)
     lt.turn_on(["turn on", 0, 0, 9, 9])
@@ -37,6 +32,5 @@ def test_switch():
     lt.turn_on(["switch", 0, 0, 9, 9])
     assert lt.count==100
 
-def test_lightester_apply():
-    pass
+
 
