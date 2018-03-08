546009807580e674eca823b02316e440e24281d1
Shauna Porter <shauna.porter@ucdconnect.ie>
2018-03-08 16:45:35 +0000

    Update to parse file for reading data

--- countMeIn/main.py
+++ countMeIn/main.py
@@ -25,14 +25,15 @@ def parse_file(filename):
     rex = re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*") 
     my_list=[int(fh1[0])]
     for line in fh1[1:]:
-        rexy = rex.match(line)
-        my_cmd=[]
-        for i in range(1,6):
-            if i==1:
-                my_cmd.append(rexy.group(i))
-            else:
-                my_cmd.append(int(rexy.group(i)))    
-        my_list.append(my_cmd)
+        rexy = rex.search(line)
+        if rexy:
+            my_cmd=[]
+            for i in range(1,6):
+                if i==1:
+                    my_cmd.append(rexy.group(i))
+                else:
+                    my_cmd.append(int(rexy.group(i)))    
+            my_list.append(my_cmd)
     return my_list
 
 def script_run():
